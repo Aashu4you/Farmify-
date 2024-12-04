fc5ea0708d410ee6aac1fc00a0df8d03120e664c
jiyasingh5725 <jiyaprab@gmail.com>
2024-12-04 20:25:02 +0530

    Create login.js

---
+++ Frontend/login.js
@@ -0,0 +1,8 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // You can add any validation or logic here
+
+    // Redirect to the new page
+    window.location.href = 'HomePage.html';
+});
